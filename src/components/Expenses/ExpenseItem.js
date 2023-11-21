9a331816dadd46eb26d7b05c04d9d8fc09aa4d1e
EC2 Default User <ec2-user@ip-10-0-0-242.ap-northeast-1.compute.internal>
2023-11-21 14:51:04 +0000

    use_State_add

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,11 @@
-
+import React,{ useState }from 'react';
 
 import ExpenseDate from '../Expenses/ExpenseDate';
 import '../Expenses/ExpenseItem.css';
 import Card from '../UI/Card';
 function ExpenseItem(props) {
+    const[title,setTitle]= useState(props.title);
+    
     //削除可能になる。
 // const expenseDate = new Date(2021, 2, 28);
  //const expenseTitle = 'Car Insurance';
@@ -12,14 +14,18 @@ function ExpenseItem(props) {
  //const month = props.date.toLocaleString('en-US', { month: 'long' });
  //const day = props.date.toLocaleString('en-US', { day: '2-digit' });
  //const year = props.date.getFullYear();
- 
+ const clickHandler = () => {
+  setTitle('update!');
+      console.log(title);
+ };
     return(
       <Card className='expense-item'>
        <ExpenseDate date={props.date}/>
         <div className= " expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">{props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Chage Title</button>
       </Card>
     );
 
